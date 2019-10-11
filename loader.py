1f2d22f8eee6768430f058551e26871dfeb8af7f
Said Id-bella ali <sid-bell@e1r5p1.1337.ma>
2019-10-11 21:34:29 +0100

    json loader

---
+++ loader.py
@@ -0,0 +1,23 @@
+import json
+import sys
+import os
+
+def loadjson():
+	argc = len(sys.argv)
+	if argc < 2 :
+		print("not config file is provided")
+		exit(1)
+	configfile = sys.argv[1]
+	if os.path.exists(configfile) == False :
+		print(configfile + " no such file or directory")
+		exit(1)
+	if os.path.isfile(configfile) == False :
+		print(configfile + " is a directory")
+		exit(1)
+	with open(configfile) as file:
+		try :
+			lst = json.load(file)
+		except :
+			print(configfile + " not a valid json file")
+			exit(1)
+	return lst
---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from loader import loadjson
+
+l = loadjson()
+print(l['socket'])
